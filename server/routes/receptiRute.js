f7ad2d9952abeaee9c5aa68a7b540b40e9b13ad0
Brizanac <45972866+Brizanac@users.noreply.github.com>
2023-05-08 00:38:32 +0200

    Delete receptiRute.js

--- server/routes/receptiRute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const receptiKontroler = require('../controllers/receptiKontroler');
-
-router.get('/', receptiKontroler.homepage);
-
-module.exports = router;
