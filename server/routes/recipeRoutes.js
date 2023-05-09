ec56b0cef4ce4c5971c966498e87cff7fb3a8436
Brizanac <45972866+Brizanac@users.noreply.github.com>
2023-05-09 17:55:07 +0200

    Update recipeRoutes.js

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -14,7 +14,7 @@ router.get('/explore-latest', recipeController.exploreLatest);
 router.get('/explore-random', recipeController.exploreRandom);
 router.get('/submit-recipe', recipeController.submitRecipe);
 router.post('/submit-recipe', recipeController.submitRecipeOnPost);
-router.get("/spoonacular", recipeController.getAllRecipes, (req, res) => {
+router.get("/lively-puce-outerwear.cyclic.app/spoonacular/", recipeController.getAllRecipes, (req, res) => {
     res.render("spoonacular", { recipes: req.recipes });
 });
 router.get("/ingredients", (req, res) => {
